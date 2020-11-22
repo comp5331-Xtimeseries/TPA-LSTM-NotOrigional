20430f4f6f90627b075251c284137e4a3da200c6
Isabella Liu <isabella618033@gmail.com>
2020-11-22 18:06:26 +0800

    changed run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 !#/bin/bash
 
-python main.py --data electricity --save electricity.pt --cuda False -e 1000 -spe 1 -nl 1 -not 168 -sl 24 -sp -max 2>&1 | tee elecLog.txt
+python3 main.py --data electricity --save electricity2.pt --cuda False -e 10 -spe 1 -nl 1 -not 168 -sl 24 -sp -max 2>&1 | tee elecLog.txt
 
